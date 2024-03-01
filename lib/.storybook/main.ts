77011c6c0c34d83241ca6e015efcea113826ed70
Peter T Bosse II <peter@ptb.dev>
2024-03-01 09:25:00 -0500

    Add Storybook configuration

---
+++ lib/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-vite"
+
+const config: StorybookConfig = {
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  core: {
+    disableTelemetry: true
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  stories: ["../src/**/*.stories.tsx"]
+}
+
+export default config
---
+++ lib/vite.config.ts
@@ -0,0 +1,6 @@
+import { default as reactPlugin } from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [reactPlugin()]
+})
