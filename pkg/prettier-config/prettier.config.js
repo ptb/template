daa77d2ae8652f71c4021ae2bc96d421099885f7
Peter T Bosse II <peter@ptb.dev>
2024-03-01 09:20:00 -0500

    Add Prettier configuration

---
+++ pkg/prettier-config/prettier.config.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  parser: "typescript",
+  printWidth: 70,
+  proseWrap: "always",
+  quoteProps: "preserve",
+  requirePragma: false,
+  semi: false,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+}
---
+++ pkg/prettier-config/types.d.ts
@@ -0,0 +1,6 @@
+import type { Config } from "prettier"
+
+declare const PrettierConfig: Config
+
+/* eslint-disable-next-line import/no-default-export */
+export default PrettierConfig
